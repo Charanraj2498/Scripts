e6beee23362046bece21c5dd0a7ba8798dd308d3
Charanraj2498 <Charan.r.555@gmail.com>
2025-01-10 06:47:50 +0530

    Update disk_usage.sh

--- disk_usage.sh
+++ disk_usage.sh
@@ -2,6 +2,6 @@
 size=$(df -h . | awk 'NR==2 {print$(NF-1)}' | sed 's/%//g')
 if [ $size -gt 25 ]
 then
-	echo "Subject:current usage is more than limit please check and do the needful" | sendmail -v charan.r.555@gmail.com
+	echo "Subject:alert" | sendmail -v charan.r.555@gmail.com
 fi
 
